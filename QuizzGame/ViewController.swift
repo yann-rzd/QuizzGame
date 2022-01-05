26b6058805438cb5110de3d20a9ba7735851113d
Yann Rouzaud <yann.rouzaud@gmail.com>
2022-01-05 16:47:03 +0100

    Initial Commit

---
+++ QuizzGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizzGame
+//
+//  Created by Yann Rouzaud on 05/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
